aecd6e72fcc4022c30268f07d713324f8516f881
Yoeri Pasmans <yoerip@live.nl>
2018-02-07 13:21:09 +0100

    Added code comments

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,6 +2,7 @@
 	'use strict';
 
 	var app = {
+		//Starts app with initialize the routes
 		init: function() {
 			routes.init();
 		}
@@ -9,18 +10,23 @@
 
 	var routes = {
 		init: function() {
+			//Toggle the sections when the hash changes. This happens when the user clicks on a link.
 			window.addEventListener("hashchange", function(route) {
-				sections.toggle(route.newURL);
+				//Splits the clicked url to a usable string with the name of the clicked hash
+				route = route.newURL.split("#")[1];
+				//Passes the hash to the sections as a parameter.
+				sections.toggle(route);
 			});
 		}
 	};
 
 	var sections = {
 		toggle: function(route) {
-			var hash = route.split("#")[1];
+			//Selects all sections 
 			var elements = document.querySelectorAll('section');
 
 			for (var i = 0; i < elements.length; i++) {
+				//If the hash is equal to an id of a section show that section. If not hide it.
 				if (elements[i].id === hash) {
 					elements[i].style.display = "block";
 				} else {
