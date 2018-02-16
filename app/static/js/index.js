86ac10ccf884fbdce0804bdb3793e5eaf76e57af
Yoeri Pasmans <yoerip@live.nl>
2018-02-16 01:50:29 +0100

    Added functionality

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -22,8 +22,7 @@
 					sections.toggle('pokemons');
 				},
 				'pokemons/:name': function(name) {
-					sections.toggle('pokemons-detail');
-					render.renderDetail(name);
+					dataHandler.requestDetail(name);
 				}
 			});
 		},
@@ -49,59 +48,95 @@
 
 	};
 
-	var dataObject;
-
 	var dataHandler = {
 
+		//Get the data from the pokemon API
 		request: function() {
 			fetch('https://pokeapi.co/api/v2/pokemon')
 				.then(function(response) {
 					return response.json();
 				})
 				.then(function(data) {
-					dataObject = data.results;
-					render.renderOverview();
+					dataHandler.data = data.results;
+					//Add an ID to every object
+					var dataObject = data.results.map(function(i, index) {
+						return {
+							id: index,
+							name: i.name,
+							url: i.url
+						};
+					});
+					render.overview(dataObject);
 				});
 		},
+
+		requestDetail: function(name) {
+			if (name) {
+				//Get the object with the name of name of the parameter and save it in variable
+				var dataDetail = this.filter(name);
+
+				//Get more data of the filtered object
+				fetch(dataDetail[0].url)
+					.then(function(response) {
+						return response.json();
+					})
+					.then(function(data) {
+						render.detail(data);
+					});
+			}
+		},
+
+		filter: function(name) {
+			var dataDetail = this.data.filter(function(obj) {
+				if (obj.name == name) {
+					return true;
+				} else {
+					return false;
+				}
+			});
+			return dataDetail;
+		},
+
+		map: function() {}
 
 	};
 
 	var render = {
 
-		renderOverview: function() {
+		overview: function(dataObject) {
 			for (var i = 0; i < dataObject.length; i++) {
 				var directives = {
 					name: {
 						text: function(params) {
 							return this.name;
 						},
+					},
+					link: {
 						href: function(params) {
 							return "#pokemons/" + this.name;
 						}
 					},
+					img: {
+						src: function(params) {
+							return "static/img/pokemons/" + (this.id + 1) + ".png";
+						}
+					}
 				};
 				Transparency.render(document.querySelector('#pokemons ul'), dataObject, directives);
 			}
 
 		},
-		renderDetail: function(name) {
-
-			for (var i = 0; i < dataObject.length; i++) {
-				if (dataObject[i].name == name) {
-					var directives = {
-						name: {
-							text: function(params) {
-								return this.name;
-							},
-							href: function(params) {
-								return "#pokemons/" + this.name;
-							}
-						},
-					};
-					Transparency.render(document.querySelector('#pokemons-detail'), dataObject[i], directives);
+		detail: function(dataObject) {
+			var directives = {
+				img: {
+					src: function(params) {
+						return "static/img/pokemons/" + this.id + ".png";
+					}
 				}
-			}
 
+			};
+			Transparency.render(document.querySelector('#pokemons-detail'), dataObject, directives);
+			sections.toggle('pokemons-detail');
 		}
 	};
 
